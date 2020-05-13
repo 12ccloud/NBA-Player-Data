867ce2e5dbefbae0f1314f66967281cd790b8d78
ccloud12 <ccloud12@12cclouds-MacBook-Air.local>
2020-05-13 16:44:30 -0700

    First commit

---
+++ components/TopBar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import logo from "../assets/images/nba-logoman-word-white.svg"
+
+class TopBar extends React.Component  {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                 <img src={logo} className="App-logo" alt="logo" />
+                </header>
+            </div>
+        );
+    }
+}
+
+export default TopBar;
+
